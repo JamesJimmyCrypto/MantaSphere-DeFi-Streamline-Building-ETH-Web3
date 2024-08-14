ca49e7c8ad4da9a144188e84734f491c1fffb209
kunaldhongade <kunaldhongade1@gmail.com>
2024-08-14 19:36:34 +0530

    Add: route for auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { register, login } = require("../controllers/authController");
+const { register, login, getUser } = require("../controllers/authController");
 const router = express.Router();
 
+// User registration
 router.post("/register", register);
+
+// User login
 router.post("/login", login);
+
+// Get user data
+router.get("/user", getUser);
 
 module.exports = router;
